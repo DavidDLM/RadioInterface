543d33520c8b73f0db81959a8f100b0cccf138a0
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2020-01-20 20:28:15 -0600

    Update FuncionesRadio.java

--- FuncionesRadio.java
+++ FuncionesRadio.java
@@ -1,7 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
+import javax.swing.Timer;
 /**
  * @author Mario de Leon
  *
@@ -11,11 +15,14 @@ public class FuncionesRadio implements RadioInterface{
 	
 	static List<String> listaAM = new ArrayList<String>();
 	static List<String> listaFM = new ArrayList<String>();
+	//static List<Integer> listaBotones = new ArrayList<Integer>();
 
 	@Override
 	public String estacionActual() {
 		// TODO Auto-generated method stub
-		return null;
+		String actual = "La estación actual la puede consultar en su pantalla. La estacion guardada se mostrara en grande al "
+				+ "seleccionar uno de los 12 botones.";
+		return actual;
 	}
 
 	@Override
@@ -27,13 +34,15 @@ public class FuncionesRadio implements RadioInterface{
 	@Override
 	public void onOff() {
 		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Bienvenido!");
 		
 	}
 
 	@Override
 	public void cambiarFrecuencia() {
 		// TODO Auto-generated method stub
-		
+		JOptionPane.showMessageDialog(null, "Las estaciones AM estan disponibles desde la 530 a la 1610. Las estaciones FM estan disponibles desde la"
+				+ " 87.9 a la 107.9.");
 	}
 
 	@Override
@@ -45,12 +54,35 @@ public class FuncionesRadio implements RadioInterface{
 	@Override
 	public void guardar(int boton) {
 		// TODO Auto-generated method stub
-		
+		if(boton < 200) {
+			JOptionPane.showMessageDialog(null, "Puede guardar hasta 12 emisoras FM distintas!");
+		}else if (boton > 200){
+			JOptionPane.showMessageDialog(null, "Puede guardar hasta 12 emisoras AM distintas!");
+		}else {
+			JOptionPane.showMessageDialog(null, "Puede guardar hasta 12 emisoras AM y 12 emisoras FM distintas!");
+		}
+
 	}
 
 	@Override
 	public void seleccionarEmisora(int boton) {
 		// TODO Auto-generated method stub
+		final int TIME_VISIBLE = 3000;
+		
+		JOptionPane pane = new JOptionPane("Sintonizando la emisora guardada...", JOptionPane.INFORMATION_MESSAGE);
+        JDialog dialog = pane.createDialog(null, "Sintonizando. Por favor espere...");
+        dialog.setModal(false);
+        dialog.setVisible(true);
+		
+
+		new Timer(TIME_VISIBLE, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				dialog.setVisible(false);
+			}
+		}).start();
 		
 	}
 	
